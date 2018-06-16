c7b952e3dee52b4bfbc9ed9ce7d1fcb3e1afda0c
LYOvQ <ly415610704@gmail.com>
2018-06-16 14:11:18 +0900

    123
    56

---
+++ Assets/daima/PlayerCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour {
+    const float LENGTH = -10.0f;
+    GameObject _Player;
+
+    // Use this for initialization
+    void Start()
+    {
+        _Player = GameObject.FindGameObjectWithTag("Player");
+        transform.position = new Vector3(_Player.transform.position.x, _Player.transform.position.y, LENGTH);
+        transform.LookAt(_Player.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(_Player.transform.position.x, _Player.transform.position.y, LENGTH);
+        transform.LookAt(_Player.transform.position);
+    }
+}
